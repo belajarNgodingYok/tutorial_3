9e140b83d16b84a38ca29d204cb6cf62913da6ea
snaid48 <teskirimemail92@gmail.com>
2018-12-04 16:17:57 +0700

    video 12

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -5,13 +5,27 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
 
 class LoginTest extends TestCase
 {
     use RefreshDatabase;
+
+    public function test_correct_response_after_user_logs_in()
+    {
+        $user = factory(User::class)->create();
+        $this->Json('POST','/login', [
+            'email' => $user->email,
+            'password' => 'secret'
+        ])->assertStatus(200)
+        ->assertJson([
+            'status' => 'ok '
+        ])->assertSessionHas('success', 'Succesfully Logged in');
+    }
+
     public function test_a_user_receive_correct_message_when_passing_in_wrong_credentials()
     {
-        $user = factory(App\User::class)->create();
+        $user = factory(User::class)->create();
         $this->postJson('/login', [
             'email' => $user->email,
             'password' => 'wrong-password'
