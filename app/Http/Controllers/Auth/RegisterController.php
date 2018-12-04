3d903fe309383ea7c4f576307710484ace7699d7
snaid48 <teskirimemail92@gmail.com>
2018-12-04 22:23:43 +0700

    video 14

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
+use Illuminate\Http\Request;
+use App\Mail\ConfirmYourEmail;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Mail;
+
 
 class RegisterController extends Controller
 {
@@ -69,5 +73,18 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+    }
+
+    /**
+     * The user has been registered.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function registered(Request $request, $user)
+    {
+        Mail::to($user)->send(new ConfirmYourEmail($user));
+        return redirect($this->redirectPath());
     }
 }
--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -2,12 +2,17 @@
 
 namespace Tests\Feature;
 
+use Mail;
 use Tests\TestCase;
+use App\Mail\ConfirmYourEmail;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+
 class RegistrationTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_a_user_has_a_default_username_after__registration()
     {
         $this->post('/register', [
@@ -20,5 +25,19 @@ class RegistrationTest extends TestCase
             'username' => str_slug('lela li'),
         ]);
 
+    }
+
+    public function test_an_email_is_sent_to_newly_registration_users()
+    {
+        $this->withoutExceptionHandling();
+        Mail::fake();
+        //register new user
+        $this->post('/register', [
+            'name' => 'lela li',
+            'email' => 'lela@gmail.com',
+            'password' => 'secret'
+        ])->assertRedirect();
+        //assert that a particular email was sent
+        Mail::assertSent(ConfirmYourEmail::class);
     }
 }
