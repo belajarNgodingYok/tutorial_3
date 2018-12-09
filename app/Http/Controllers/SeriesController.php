444d46ead5531ae517ae370c07f0cb0bed6ca19c
snaid48 <teskirimemail92@gmail.com>
2018-12-09 21:21:52 +0700

    video 46

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Series;
 use App\Http\Requests\CreateSeriesRequest;
+use App\Http\Requests\UpdateSeriesRequest;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
@@ -64,9 +65,9 @@ class SeriesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Series $series)
     {
-        //
+        return view('admin.series.edit')->withSeries($series);
     }
 
     /**
@@ -76,9 +77,19 @@ class SeriesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UpdateSeriesRequest $request, Series $series)
     {
-        //
+        if ($request->hasFile('image')) {
+            $series->image_url = $request->uploadSeriesImage()->fileName;
+        }
+
+        $series->title = $request->title;
+        $series->description = $request->description;
+        $series->slug = str_slug($request->title);
+
+        $series->save();
+
+        return redirect()->route('series.index');
     }
 
     /**
